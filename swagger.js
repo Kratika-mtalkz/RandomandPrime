9379239d6479d64f949c2ce69e2f6898f330f7f2
Kratika Kaushik <kratikakaushik27@gmail.com>
2023-02-15 23:13:00 +0530

    swagger

--- swagger.js
+++ swagger.js
@@ -19,7 +19,7 @@ const swaggerConfig = () => {
         url: "https://swagger.io",
         description: "Find more info here",
       },
-      schemes: ["http"],
+    //  schemes: ["http"],
       consumes: ["application/json"],
       produces: ["application/json"],
     },
